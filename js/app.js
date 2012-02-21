197615732b7787b68b665dcb50e2df61d3aaf39e
Evan You <yyx990803@gmail.com>
2012-02-21 08:34:43 -0500

    mod

--- js/app.js
+++ js/app.js
@@ -858,30 +858,7 @@ Home.prototype = {
 	}
 };
 
-var app = new Home([
-	new List('Hello', [
-		new Todo('Swipe right to complete'),
-		new Todo('Swipe left to delete'),
-		new Todo('Tap on text to edit'),
-		new Todo('Long tap to change order'),
-		new Todo('Drag down to add new'),
-		new Todo('Pinch in to go back.')
-	]),
-	new List('This is a demo', [
-		new Todo('Built with HTML5'),
-		new Todo('CSS3'),
-		new Todo('and Zepto.js')
-	]),
-	new List('by Evan You', [
-		new Todo('@youyuxi'),
-		new Todo('By the way'),
-		new Todo('I\'m looking for a job!'),
-		new Todo('youyuxi.com')
-	])
-]);
-
 $(function(){
-	
 	//setup scrolling
 	$(document)
 	.bind('touchmove', function(e){
@@ -894,6 +871,28 @@ $(function(){
 	.bind('touchend touchcancel', function(e){
 		window.globalDrag = false;
 	});
+	
+	var app = new Home([
+		new List($.os.ios ? 'Hello':'This works better on iOS', [
+			new Todo('Swipe right to complete'),
+			new Todo('Swipe left to delete'),
+			new Todo('Tap on text to edit'),
+			new Todo('Long tap to change order'),
+			new Todo('Drag down to add new'),
+			new Todo('Pinch in to go back.')
+		]),
+		new List('This is a demo', [
+			new Todo('Built with HTML5'),
+			new Todo('CSS3'),
+			new Todo('and Zepto.js')
+		]),
+		new List('by Evan You', [
+			new Todo('@youyuxi'),
+			new Todo('By the way'),
+			new Todo('I\'m looking for a job!'),
+			new Todo('youyuxi.com')
+		])
+	]);
 	
 	app.render().appendTo('#wrapper');
 	
